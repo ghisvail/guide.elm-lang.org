56071c29abe94ba4834f6fc23c4b3b3b77f7fdf4
Nicolas Perriault <nicolas@perriault.net>
2022-04-04 15:42:46 +0200

    Add page source & edit github links.

--- honkit-plugin-french-typography/index.js
+++ honkit-plugin-french-typography/index.js
@@ -1,8 +1,17 @@
+const BASE_GITHUB_URL = "https://github.com/elm-france/guide.elm-lang.org";
+
 module.exports = {
   // Map of hooks
   hooks: {
     "page:before": function (page) {
+      // Replace non-breaking spaces by their html entity
       page.content = page.content.replaceAll(" ", "&nbsp;");
+      // Add page source & edit links
+      page.content += `
+---
+
+[Source de cette page](${BASE_GITHUB_URL}/tree/master/book/${page.path}) —
+[Suggérer une modification](${BASE_GITHUB_URL}/edit/master/book/${page.path})`;
       return page;
     },
   },
